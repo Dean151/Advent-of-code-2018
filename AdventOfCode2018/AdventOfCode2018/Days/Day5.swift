f1760b479f339ee88c6110f70e31fea3a790f50e
Dean151 <thomas.durand.verjat@gmail.com>
2018-12-05 06:48:57 +0100

    Setting up Day 5

---
+++ AdventOfCode2018/AdventOfCode2018/Days/Day5.swift
@@ -0,0 +1,16 @@
+//
+//  Day5.swift
+//  AdventOfCode2018
+//
+//  Created by Thomas Durand on 05/12/2018.
+//  Copyright © 2018 Thomas Durand. All rights reserved.
+//
+
+import Foundation
+
+class Day5 {
+    
+    static func run() {
+        let input = try! Input.get("day5.txt")
+    }
+}
--- AdventOfCode2018/AdventOfCode2018/main.swift
+++ AdventOfCode2018/AdventOfCode2018/main.swift
@@ -8,7 +8,8 @@
 
 import Foundation
 
-Day1.run()
-Day2.run()
-Day3.run()
-Day4.run()
+// Day1.run()
+// Day2.run()
+// Day3.run()
+// Day4.run()
+Day5.run()
