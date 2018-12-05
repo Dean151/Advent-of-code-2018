8c2c2fdaa9e8522b8d759d3eab6d5f9327a81e09
Dean151 <thomas.durand.verjat@gmail.com>
2018-12-05 07:07:53 +0100

    Solved Day 5-1

--- AdventOfCode2018/AdventOfCode2018/Days/Day5.swift
+++ AdventOfCode2018/AdventOfCode2018/Days/Day5.swift
@@ -12,5 +12,28 @@ class Day5 {
     
     static func run() {
         let input = try! Input.get("day5.txt")
+        var string = input.components(separatedBy: .newlines).first!
+        
+        var i = string.startIndex
+        while (i < string.index(before: string.endIndex)) {
+            let a = Int(string[i].unicodeScalars.first!.value)
+            let b = Int(string[string.index(after: i)].unicodeScalars.first!.value)
+            
+            if abs(a - b) == 32 {
+                // Capitals and simple are separated from 32 unicode numbers exactly
+                // We remove the elements, and we keep going
+                string.remove(at: string.index(after: i))
+                string.remove(at: i)
+                // We move backward (if possible)
+                if i != string.startIndex {
+                    i = string.index(before: i)
+                }
+            } else {
+                // We move forward
+                i = string.index(after: i)
+            }
+        }
+        
+        print("Left over of the polymer for Day 5-1 is \(string.count)")
     }
 }
