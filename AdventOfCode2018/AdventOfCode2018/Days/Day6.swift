d27fafb2f16b9a412c848b8fd998cf83d646055b
Dean151 <thomas.durand.verjat@gmail.com>
2018-12-06 06:42:02 +0100

    Starting Day 6 puzzles

---
+++ AdventOfCode2018/AdventOfCode2018/Days/Day6.swift
@@ -0,0 +1,16 @@
+//
+//  Day6.swift
+//  AdventOfCode2018
+//
+//  Created by Thomas Durand on 06/12/2018.
+//  Copyright © 2018 Thomas Durand. All rights reserved.
+//
+
+import Foundation
+
+class Day6: Day {
+    
+    static func run() {
+        let input = try! Input.get("day6.txt")
+    }
+}
