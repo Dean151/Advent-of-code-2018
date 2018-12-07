c56c185581a185d3151f117552b60167333c7f9d
Dean151 <thomas.durand.verjat@gmail.com>
2018-12-07 07:21:55 +0100

    Starting Day 7

---
+++ AdventOfCode2018/AdventOfCode2018/Days/Day7.swift
@@ -0,0 +1,16 @@
+//
+//  Day7.swift
+//  AdventOfCode2018
+//
+//  Created by Thomas Durand on 07/12/2018.
+//  Copyright © 2018 Thomas Durand. All rights reserved.
+//
+
+import Foundation
+
+class Day7: Day {
+    
+    static func run(input: String) {
+        
+    }
+}
--- AdventOfCode2018/AdventOfCode2018/main.swift
+++ AdventOfCode2018/AdventOfCode2018/main.swift
@@ -8,9 +8,10 @@
 
 import Foundation
 
-Day1.solve()
-Day2.solve()
-Day3.solve()
-Day4.solve()
-Day5.solve()
-Day6.solve()
+//Day1.solve()
+//Day2.solve()
+//Day3.solve()
+//Day4.solve()
+//Day5.solve()
+//Day6.solve()
+Day7.solve()
