aa91b1b76cf0ad19f5ff67bbc9722e368ba566fc
Dean151 <thomas.durand.verjat@gmail.com>
2018-12-08 13:51:52 +0100

    Starting Day 8

---
+++ AdventOfCode2018/AdventOfCode2018/Days/Day8.swift
@@ -0,0 +1,16 @@
+//
+//  Day8.swift
+//  AdventOfCode2018
+//
+//  Created by Thomas Durand on 08/12/2018.
+//  Copyright © 2018 Thomas Durand. All rights reserved.
+//
+
+import Foundation
+
+class Day8: Day {
+    
+    static func run(input: String) {
+        
+    }
+}
