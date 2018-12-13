b45c7deff4db03c30814c343363f99ac36709416
Thomas Durand <thomas.durand.verjat@gmail.com>
2018-12-13 10:44:42 +0100

    Starting Day 13

---
+++ AdventOfCode2018/AdventOfCode2018/Days/Day13.swift
@@ -0,0 +1,16 @@
+//
+//  Day13.swift
+//  AdventOfCode2018
+//
+//  Created by Thomas DURAND on 13/12/2018.
+//  Copyright © 2018 Thomas Durand. All rights reserved.
+//
+
+import Foundation
+
+class Day13: Day {
+    
+    static func run(input: String) {
+        
+    }
+}
