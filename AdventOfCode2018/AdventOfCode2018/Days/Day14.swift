c80bfd5b3783b61a34680c1826da4ef8d0619e95
Dean151 <thomas.durand.verjat@gmail.com>
2018-12-14 09:32:33 +0100

    Solved Day 14-2 ; not really efficient, but working...

--- AdventOfCode2018/AdventOfCode2018/Days/Day14.swift
+++ AdventOfCode2018/AdventOfCode2018/Days/Day14.swift
@@ -11,29 +11,46 @@ import Foundation
 class Day14: Day {
     
     static func run(input: String) {
-        let numberOfRecipies = Int(input.components(separatedBy: .newlines).first!)!
+        let toFind = input.components(separatedBy: .newlines).first!
+        let numberOfRecipies = Int(toFind)!
         
         var recipies = [3, 7]
         var currentA = 0
         var currentB = 1
         
-        while recipies.count < numberOfRecipies + 10 {
+        var part1done = false
+        var part2done = false
+        
+        while !part1done || !part2done {
             let a = recipies[currentA]
             let b = recipies[currentB]
             
             let c = a + b
+            let added: Int
             if c < 10 {
                 recipies.append(c)
+                added = 1
             } else {
                 recipies.append(c / 10)
                 recipies.append(c % 10)
+                added = 2
+            }
+            
+            if !part1done && recipies.count >= numberOfRecipies + 10 {
+                let next = [Int](0...9).reduce(0, { return 10 * $0 + recipies[numberOfRecipies + $1] })
+                print("Score of next ten recipies for Day 14-1 are \(next)")
+                part1done = true
+            }
+            
+            let suffix = recipies.suffix(toFind.count + (added - 1)).reduce("", { $0 + "\($1)" })
+            if !part2done && suffix.contains(toFind) {
+                let correct = suffix.hasPrefix(toFind) && added == 2 ? 1 : 0
+                print("Input found after making \(recipies.count - correct - toFind.count) recipies for Day 14-2")
+                part2done = true
             }
             
             currentA = (currentA + 1 + a) % recipies.count
             currentB = (currentB + 1 + b) % recipies.count
         }
-        
-        let next = [Int](0...9).reduce(0, { return 10 * $0 + recipies[numberOfRecipies + $1] })
-        print("Score of next ten recipies for Day 14-1 are \(next)")
     }
 }
--- readme.md
+++ readme.md
@@ -33,5 +33,6 @@ Some attention will be ported to performance consideration, let try to make that
 | **11** | 1,879s          | 21,93    | 231,108,14 |
 | **12** | 0,029s          | 3337     | 4300000000349 |
 | **13** | 0,030s          | 124,90   | 145,88     |
+| **14** | **41,748s**     | 6910849249 | 20330673 |
 
 \* : Best elapsed time of the complete solution for my MacBook Pro 13in mid 2013 in "Release" mode
