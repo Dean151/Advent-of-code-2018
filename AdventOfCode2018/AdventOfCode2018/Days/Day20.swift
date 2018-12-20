806bf28f7960773c2e5e7e094468cc16902bf081
Thomas Durand <thomas.durand.verjat@gmail.com>
2018-12-20 12:38:22 +0100

    Starting up a logic of parsing the place ^^

--- AdventOfCode2018/AdventOfCode2018/Days/Day20.swift
+++ AdventOfCode2018/AdventOfCode2018/Days/Day20.swift
@@ -10,30 +10,126 @@ import Foundation
 
 class Day20: Day {
     
-    static func fartestPath(for regex: String) -> String {
-        var regex = regex.trimmingCharacters(in: CharacterSet(charactersIn: "^$"))
-        while let end = regex.firstIndex(of: ")") {
-            // Find the corresponding opening (
-            guard let start = regex[...end].lastIndex(of: "(") else {
-                fatalError("Closing ) does not have it's (")
-            }
-            let subregex = regex[regex.index(after: start)...regex.index(before: end)]
-            regex.replaceSubrange((start...end).relative(to: regex), with: fartestPath(for: String(subregex)))
+    struct Position: Hashable {
+        var x: Int, y: Int
+        
+        var hashValue: Int {
+            return "\(x),\(y)".hashValue
         }
-        let fartest = regex.components(separatedBy: "|").max(by: { $0.count < $1.count })!
-        print(fartest)
-        return fartest
+    }
+    
+    class Facility: CustomStringConvertible {
+        
+        var squares = [Int: [Int: Square]]()
+        
+        var yMin: Int {
+            return squares.keys.min() ?? 0
+        }
+        var yMax: Int {
+            return squares.keys.max() ?? 0
+        }
+        var xMin: Int {
+            return squares.mapValues({ $0.keys.min() ?? 0 }).values.min() ?? 0
+        }
+        var xMax: Int {
+            return squares.mapValues({ $0.keys.max() ?? 0 }).values.max() ?? 0
+        }
+        
+        var rooms = [Position: Int]()
+        
+        enum Square: String {
+            case room = "."
+            case door = "+"
+            case wall = "#"
+        }
+        
+        enum Direction: String {
+            case north = "N", west = "W", east = "E", south = "S"
+        }
+        
+        subscript(pos: Position) -> Square {
+            get {
+                return squares[pos.y]?[pos.x] ?? .wall
+            }
+            set {
+                if var line = squares[pos.y] {
+                    line.updateValue(newValue, forKey: pos.x)
+                    squares.updateValue(line, forKey: pos.y)
+                } else {
+                    squares.updateValue([pos.x: newValue], forKey: pos.y)
+                }
+            }
+        }
+        
+        init(regex: String) {
+            // Initialize stuff
+            self[Position(x: 0, y: 0)] = .room
+            rooms[Position(x: 0, y: 0)] = 0
+            
+            // We parse it twice to update distances with the minimum possible :)
+            for _ in 1...2 {
+                var pos = Position(x: 0, y: 0)
+                goThrewDoor(from: &pos, direction: .west)
+                goThrewDoor(from: &pos, direction: .north)
+                goThrewDoor(from: &pos, direction: .east)
+            }
+            
+            print(self)
+        }
+        
+        func goThrewDoor(from pos: inout Position, direction: Direction) {
+            let distance = rooms[pos] ?? 0
+            move(from: &pos, direction: direction)
+            self[pos] = .door
+            move(from: &pos, direction: direction)
+            self[pos] = .room
+            
+            if let oldDistance = rooms[pos] {
+                rooms[pos] = min(distance + 1, oldDistance)
+            } else {
+                rooms[pos] = distance + 1
+            }
+        }
+        
+        func move(from pos: inout Position, direction: Direction) {
+            switch direction {
+            case .west:
+                pos.x -= 1
+            case .east:
+                pos.x += 1
+            case .north:
+                pos.y -= 1
+            case .south:
+                pos.y += 1
+            }
+        }
+        
+        var description: String {
+            var string = ""
+            for y in yMin-1...yMax+1 {
+                for x in xMin-1...xMax+1 {
+                    let pos = Position(x: x, y: y)
+                    string += self.rooms[pos]?.description ?? self[pos].rawValue
+                }
+                string += "\n"
+            }
+            return string
+        }
+        
+        var fartest: Int {
+            return rooms.values.max() ?? 0
+        }
     }
     
     static func run(input: String) {
         let regex = input.components(separatedBy: .newlines).first!
         
-        assert(fartestPath(for: "^WNE$").count == 3)
-        assert(fartestPath(for: "^ENWWW(NEEE|SSE(EE|N))$").count == 10)
-        assert(fartestPath(for: "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$").count == 18)
-        assert(fartestPath(for: "^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$").count == 23)
-        assert(fartestPath(for: "^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$").count == 31)
+        assert(Facility(regex: "^WNE$").fartest == 3)
+        assert(Facility(regex: "^ENWWW(NEEE|SSE(EE|N))$").fartest == 10)
+        //assert(Facility(regex: "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$").fartest == 18)
+        //assert(Facility(regex: "^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$").fartest == 23)
+        //assert(Facility(regex: "^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$").fartest == 31)
         
-        print("Max number of doors we can pass threw for Day 20-1 is \(fartestPath(for: regex).count)")
+        //print("Max number of doors we can pass threw for Day 20-1 is \(Facility(regex: regex).fartest)")
     }
 }
