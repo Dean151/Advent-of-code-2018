ddd806a6d78c69e3d8aca3deadb5b337f3d944b6
Dean151 <thomas.durand.verjat@gmail.com>
2018-12-22 18:52:48 +0100

    I need a real infinite cave...

--- AdventOfCode2018/AdventOfCode2018/Days/Day22.swift
+++ AdventOfCode2018/AdventOfCode2018/Days/Day22.swift
@@ -12,8 +12,16 @@ class Day22: Day {
     
     typealias Position = (x: Int, y: Int)
     
-    struct Cavern {
-        
+    enum Tool {
+        case none, torch, climbingGear
+    }
+    
+    struct Node: Hashable {
+        let index: Int
+        let tool: Tool
+    }
+    
+    struct Cave {
         enum Region: Int, CustomStringConvertible {
             case rocky = 0, wet, narrow
             
@@ -27,11 +35,22 @@ class Day22: Day {
                     return "|"
                 }
             }
+            
+            var validTools: Set<Tool> {
+                switch self {
+                case .rocky:
+                    return Set<Tool>([.torch, .climbingGear])
+                case .wet:
+                    return Set<Tool>([.none, .climbingGear])
+                case .narrow:
+                    return Set<Tool>([.none, .torch])
+                }
+            }
         }
         
         let width: Int
         let height: Int
-        let target: Position
+        let target: Node
         let regions: [Int: Region]
         
         static func index(at pos: Position, width: Int) -> Int {
@@ -39,20 +58,20 @@ class Day22: Day {
         }
         
         func index(at pos: Position) -> Int {
-            return Cavern.index(at: pos, width: width)
+            return Cave.index(at: pos, width: width)
         }
         
         init(target: Position, depth: Int) {
-            let width = target.x + 10
-            let height = target.y + 10
+            let width = target.x * 5
+            let height = target.y + 50
             
             let capacity = width * height
             var erosions = [Int: Int](minimumCapacity: capacity)
             var regions = [Int: Region](minimumCapacity: capacity)
             
-            for y in 0...target.y {
-                for x in 0...target.x {
-                    let index = Cavern.index(at: (x: x, y: y), width: width)
+            for y in 0..<height {
+                for x in 0..<width {
+                    let index = Cave.index(at: (x: x, y: y), width: width)
                     let geoIndex: Int
                     if (x == 0 && y == 0) || (x == target.x && y == target.y) {
                         geoIndex = 0
@@ -71,17 +90,122 @@ class Day22: Day {
             
             self.width = width
             self.height = height
-            self.target = target
+            self.target = Node(index: Cave.index(at: target, width: width), tool: .torch)
             self.regions = regions
         }
         
         var riskLevel: Int {
-            return regions.reduce(0, { $0 + ($1.key <= index(at: target) ? $1.value.rawValue : 0) })
+            let pos = position(at: target.index)
+            return regions.filter({
+                let p = position(at: $0.key)
+                return p.x <= pos.x && p.y <= pos.y
+            }).map({ $0.value.rawValue }).reduce(0, +)
+        }
+        
+        func neighbors(from node: Node) -> [(node: Node, upcost: Int)] {
+            
+            let pos = position(at: node.index)
+            if pos.x == width - 1 {
+                fatalError("Edge is too close...")
+            }
+            if pos.y == height - 1 {
+                fatalError("We need to go deeper...")
+            }
+            
+            let indexes = [
+                (x: pos.x, y: pos.y - 1),
+                (x: pos.x - 1, y: pos.y),
+                (x: pos.x + 1, y: pos.y),
+                (x: pos.x, y: pos.y + 1),
+            ].filter({ $0.x >= 0 && $0.y >= 0 }).map({ index(at: $0) })
+            
+            var neighbors = [(node: Node, upcost: Int)]()
+            
+            // We can change our tool
+            for newTool in regions[node.index]!.validTools where newTool != node.tool {
+                neighbors.append((node: Node(index: node.index, tool: newTool), upcost: 7))
+            }
+            
+            // We can move to regions where our tool is valid
+            for newIndex in indexes where regions[newIndex]!.validTools.contains(node.tool) {
+                neighbors.append((node: Node(index: newIndex, tool: node.tool), upcost: 1))
+            }
+            return neighbors
+        }
+        
+        func position(at index: Int) -> Position {
+            let x = index % width, y = (index - x) / width
+            return (x: x, y: y)
+        }
+        
+        func distanceFromTarget(to node: Node) -> Int {
+            let a = position(at: node.index)
+            let b = position(at: target.index)
+            let distance = abs(b.x - a.x) + abs(b.y - a.y)
+            return distance
         }
         
         var minimumReachTime: Int {
-            // TODO!
-            return 0
+            
+            // A* algorithm :)
+            var toVisit = [Node: (cost: Int, heuristic: Int)]()
+            var visited = [Node: Int]()
+            
+            // Adding the starting block
+            let start = Node(index: 0, tool: .torch)
+            toVisit[start] = (cost: 0, heuristic: 0)
+            
+            while let (node, (cost, _)) = toVisit.min(by: { $0.value.heuristic < $1.value.heuristic }) {
+                toVisit.removeValue(forKey: node)
+                
+                if (node == target) {
+                    print("Found path with cost : \(cost)")
+                    return cost
+                }
+                
+                for (newNode, upcost) in neighbors(from: node) {
+                    let newCost = cost + upcost
+                    if let alreadyVisited = visited[newNode], alreadyVisited < newCost {
+                        continue
+                    }
+                    if let inVisitList = toVisit[newNode], inVisitList.cost < newCost {
+                        continue
+                    }
+                    toVisit[newNode] = (cost: newCost, heuristic: newCost + distanceFromTarget(to: newNode))
+                }
+                
+                visited[node] = cost
+            }
+            
+            fatalError("Target not reachable")
+        }
+        
+        func printRegion() {
+            var string = ""
+            for y in 0..<height {
+                for x in 0..<width {
+                    let index = self.index(at: (x: x, y: y))
+                    string += regions[index]!.description
+                }
+                string += "\n"
+            }
+            print(string)
+        }
+        
+        func printCost(_ visited: [Node: Int]) {
+            var string = ""
+            for y in 0..<height {
+                for x in 0..<width {
+                    let index = self.index(at: (x: x, y: y))
+                    if let point = visited.filter({ $0.key.index == index }).min(by: { $0.value < $1.value }) {
+                        string += "\(point.value)\t"
+                    } else {
+                        string += "-\t"
+                    }
+                }
+                string += "\n"
+            }
+            print(string)
         }
     }
     
@@ -93,12 +217,12 @@ class Day22: Day {
         let target = (x: coords.first!, y: coords.last!)
         
         // Create the cavern
-        let cavern = Cavern(target: target, depth: depth)
+        let cavern = Cave(target: target, depth: depth)
         
-        assert(Cavern(target: (x: 10, y: 10), depth: 510).riskLevel == 114)
+        assert(Cave(target: (x: 10, y: 10), depth: 510).riskLevel == 114)
         print("Risk level of the cavern for Day 21-1 is \(cavern.riskLevel)")
         
-        assert(Cavern(target: (x: 10, y: 10), depth: 510).minimumReachTime == 45)
+        assert(Cave(target: (x: 10, y: 10), depth: 510).minimumReachTime == 45)
         print("Minimum time to reach the target for Day 21-2 is \(cavern.minimumReachTime)")
     }
 }
