492b2e48998d288f871eec008c4abd66838076d8
Dean151 <thomas.durand.verjat@gmail.com>
2018-12-24 15:48:20 +0100

    Starting the logic of parsing units

--- AdventOfCode2018/AdventOfCode2018/Days/Day24.swift
+++ AdventOfCode2018/AdventOfCode2018/Days/Day24.swift
@@ -11,35 +11,66 @@ import Foundation
 class Day24: Day {
     
     class Group {
-        typealias Attack = (type: AttackType, damage: Int)
-        
         enum Team {
-            case immuneSystem
-            case infection
+            case immuneSystem, infection
         }
         
         enum AttackType: String {
             case bludgeoning, cold, fire, radiation, slashing
         }
         
+        typealias Attack = (type: AttackType, damage: Int)
+        
         let id: Int
         let team: Team
         let initiative: Int
         let attack: Attack
+        let immunities: Set<AttackType>
+        let weaknesses: Set<AttackType>
         let hitPoints: Int
         var numberOfUnits: Int
         
-        init(id: Int, team: Team, initiative: Int, attack: Attack, hitPoints: Int, initialNumberOfUnits: Int) {
+        init(id: Int, team: Team, initiative: Int, attack: Attack, immunities: Set<AttackType>, weaknesses: Set<AttackType>, hitPoints: Int, initialNumberOfUnits: Int) {
             self.id = id
             self.team = team
             self.initiative = initiative
             self.attack = attack
+            self.immunities = immunities
+            self.weaknesses = weaknesses
             self.hitPoints = hitPoints
             self.numberOfUnits = initialNumberOfUnits
         }
+        
+        static func from(line: String, team: Team) -> Group? {
+            // TODO: parse !
+            print(line)
+            print(team)
+            return nil
+        }
+    }
+    
+    static func parseUnits(input: String) -> [Group] {
+        var groups = [Group]()
+        var team = Group.Team.immuneSystem
+        for line in input.components(separatedBy: .newlines) where !line.isEmpty {
+            if line == "Immune System:" {
+                team = .immuneSystem
+                continue
+            }
+            if line == "Infection:" {
+                team = .infection
+                continue
+            }
+            guard let group = Group.from(line: line, team: team) else {
+                continue
+            }
+            groups.append(group)
+        }
+        return groups
     }
     
     static func run(input: String) {
-        
+        let units = parseUnits(input: input)
+        print(units)
     }
 }
