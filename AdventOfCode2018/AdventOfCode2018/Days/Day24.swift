93a80b7bdb628fc26c900ca7f40f8594d8ffee36
Dean151 <thomas.durand.verjat@gmail.com>
2018-12-24 15:39:19 +0100

    Structuring Day 24

--- AdventOfCode2018/AdventOfCode2018/Days/Day24.swift
+++ AdventOfCode2018/AdventOfCode2018/Days/Day24.swift
@@ -10,6 +10,35 @@ import Foundation
 
 class Day24: Day {
     
+    class Group {
+        typealias Attack = (type: AttackType, damage: Int)
+        
+        enum Team {
+            case immuneSystem
+            case infection
+        }
+        
+        enum AttackType: String {
+            case bludgeoning, cold, fire, radiation, slashing
+        }
+        
+        let id: Int
+        let team: Team
+        let initiative: Int
+        let attack: Attack
+        let hitPoints: Int
+        var numberOfUnits: Int
+        
+        init(id: Int, team: Team, initiative: Int, attack: Attack, hitPoints: Int, initialNumberOfUnits: Int) {
+            self.id = id
+            self.team = team
+            self.initiative = initiative
+            self.attack = attack
+            self.hitPoints = hitPoints
+            self.numberOfUnits = initialNumberOfUnits
+        }
+    }
+    
     static func run(input: String) {
         
     }
